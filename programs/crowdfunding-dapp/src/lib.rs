97aff7d36f361a16ab34c74fc58d8ae0b5c4f125
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2024-01-18 13:51:59 +0530

    context of create

--- programs/crowdfunding-dapp/src/lib.rs
+++ programs/crowdfunding-dapp/src/lib.rs
@@ -6,10 +6,16 @@ declare_id!("EYH93EyjromEmPgJfp6918j7uMBMc1YKb41qVMbS5zst");
 pub mod crowdfunding_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, name: String, Description: String) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=9000, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
