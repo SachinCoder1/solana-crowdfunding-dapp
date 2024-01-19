16c99f4eaf0734c527606ed3a5afa009089bec73
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2024-01-19 14:11:01 +0530

    donate function, deploying solana program

--- programs/crowdfunding-dapp/src/lib.rs
+++ programs/crowdfunding-dapp/src/lib.rs
@@ -1,10 +1,10 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("EYH93EyjromEmPgJfp6918j7uMBMc1YKb41qVMbS5zst");
+declare_id!("3zWYuSfuCBo5AybttxXfEwQ983tFcMhhVKK5t9PnD9Jn");
 
 #[program]
 pub mod crowdfunding_dapp {
-    use anchor_lang::solana_program::entrypoint::ProgramResult;
 
     use super::*;
 
@@ -17,7 +17,7 @@ pub mod crowdfunding_dapp {
         Ok(())
     }
 
-    pub fn withdraw (ctx: Context<Withdraw>, amount:u64) -> ProgramResult {
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
         let campaign = &mut ctx.accounts.campaign;
         let user = &mut ctx.accounts.user;
 
@@ -36,6 +36,25 @@ pub mod crowdfunding_dapp {
 
         Ok(())
     }
+
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> ProgramResult {
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.user.key(),
+            &ctx.accounts.campaign.key(),
+            amount,
+        );
+
+        let _ = anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ],
+        );
+
+        (&mut ctx.accounts.campaign).amount_donated += amount;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -52,14 +71,22 @@ pub struct Withdraw<'info> {
     #[account(mut)]
     pub campaign: Account<'info, Campaign>,
     #[account(mut)]
-    pub user: Signer<'info>
+    pub user: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct Campaign {
     pub admin: Pubkey,
     pub name: String,
     pub description: String,
-    amount_donated: u64
+    amount_donated: u64,
 }
